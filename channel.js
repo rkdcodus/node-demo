99b5cd9c68c4ca75d023a829d5e7775c9bb34b02
rkdcodus <rcd7275@naver.com>
2024-11-26 13:05:31 +0900

    feat(channel.js): 채널명 개별 put api 구현

--- channel.js
+++ channel.js
@@ -38,5 +38,25 @@ app
     if (channelsDB.has(id)) res.status(200).json(channelsDB.get(id));
     else res.status(404).json({ message: "요청하신 id와 일치하는 채널이 없습니다." });
   })
-  .put((req, res) => {})
+  .put((req, res) => {
+    const id = +req.params.id;
+    const newChannelTitle = req.body.channelTitle;
+
+    if (!channelsDB.has(id)) {
+      return res.status(404).json({ message: "요청하신 id와 일치하는 채널이 없습니다." });
+    }
+
+    if (!newChannelTitle) return res.status(400).json({ message: "채널명을 입력해주세요" });
+
+    const channelTitle = channelsDB.get(id).channelTitle;
+
+    if (channelTitle === newChannelTitle) {
+      return res.status(409).json({ message: "이전 채널명과 동일합니다." });
+    }
+
+    channelsDB.set(id, { newChannelTitle });
+    res
+      .status(200)
+      .send(`채널명이 "${channelTitle}"에서 "${newChannelTitle}"(으)로 변경되었습니다.`);
+  })
   .delete((req, res) => {});
