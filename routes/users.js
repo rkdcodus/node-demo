1030f5d4582c746659415e485713452fcacfece1
rkdcodus <rcd7275@naver.com>
2024-12-03 15:20:24 +0900

    feat(users.js): Map 객체 db 대신 mariadb 연결

--- routes/users.js
+++ routes/users.js
@@ -1,75 +1,71 @@
 const express = require("express");
 const router = express.Router();
 
-const usersDB = new Map();
+const conn = require("../mariadb");
 
-// 로그인
 router.use(express.json());
+
 router.post("/signin", (req, res) => {
-  const { userId, password } = req.body;
+  const { email, password } = req.body;
 
-  if (userId && password) {
-    let isUser = false;
+  if (!email && !password) return res.status(400).json({ message: "이메일를 입력해주세요." });
+  if (!password) return res.status(400).json({ message: "비밀번호를 입력해주세요." });
 
-    usersDB.forEach((user) => {
-      if (user.userId === userId && user.password === password) {
-        res.status(200).json({ message: `${user.name}님 로그인에 성공했습니다.` });
-        isUser = true;
-      } else if (user.userId === userId) {
-        res.status(403).json({ message: "비밀번호를 잘못 입력했습니다." });
-        isUser = true;
-      }
-    });
+  conn.query("SELECT * FROM users WHERE email = ?", email, function (err, result) {
+    const user = result[0];
 
-    if (!isUser) res.status(403).json({ message: "등록되지 않은 아이디입니다." });
-  } else if (userId === "") {
-    res.status(400).json({ message: "아이디를 입력해주세요" });
-  } else if (password === "") {
-    res.status(400).json({ message: "비밀번호를 입력해주세요" });
-  }
+    if (user && password === result[0].password) {
+      res.status(200).json({ message: `${result[0].name}님 로그인 되었습니다.` });
+    } else {
+      res.status(404).json({ message: "이메일 또는 비밀번호를 잘못 입력했습니다." });
+    }
+  });
 });
 
-// 회원가입
 router.post("/join", (req, res) => {
-  const { userId, name, password } = req.body;
+  const { email, name, password, connect } = req.body;
 
-  if (userId && name && password) {
-    usersDB.forEach((user) => {
-      if (user.userId === userId) {
-        return res.status(409).json({ message: "이미 사용 중인 아이디입니다." });
+  if (email && name && password) {
+    conn.query(
+      "INSERT INTO users (email, name, password, connect) VALUES(?,?,?,?)",
+      [email, name, password, connect],
+      function (err) {
+        if (err) {
+          res.status(409).json({ message: "이미 사용 중인 아이디입니다." });
+        } else {
+          res.status(201).send({ message: `${name}님 회원가입 되었습니다.` });
+        }
       }
-    });
-
-    usersDB.set(userId, { userId, name, password });
-    res.status(201).send(`${name}님 환영합니다.`);
+    );
   } else {
     res.status(400).json({ message: "잘못된 요청입니다." });
   }
 });
 
-// 동일한 path 요청은 route로 합치기
 router
   .route("/users")
   .get((req, res) => {
-    // 회원 개별 조회
-    const { userId } = req.body;
+    const { email } = req.body;
 
-    if (usersDB.has(userId)) {
-      res.status(200).json(usersDB.get(userId));
-    } else {
-      res.status(404).json({ message: "요청하신 id 값과 일치하는 데이터가 없습니다." });
-    }
+    conn.query("SELECT * FROM users WHERE email = ?", email, function (err, result) {
+      if (result.length) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ message: "회원 정보를 찾을 수 없습니다." });
+      }
+    });
   })
   .delete((req, res) => {
-    // 회원 개별 탈퇴
-    const { userId } = req.body;
+    const { email } = req.body;
 
-    if (usersDB.has(userId)) {
-      const name = usersDB.get(userId).name;
-      usersDB.delete(userId);
-      res.status(200).json({ message: `${name}님 정상적으로 회원 탈퇴되었습니다.` });
+    if (email) {
+      conn.query("DELETE FROM users WHERE email = ?", email, function (err, result) {
+        res.status(200).json({ message: `정상적으로 회원 탈퇴되었습니다.` });
+
+        // res.status(404).json({ message: "회원 정보를 찾을 수 없습니다." });
+      });
     } else {
-      res.status(400).json({ message: `입력하신 id값과 일치하는 회원 정보가 없습니다.` });
+      res.status(400).json({ message: "이메일을 입력해주세요" });
     }
   });
 
