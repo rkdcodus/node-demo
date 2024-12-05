fc3f36393ef9c383c6860fb69257a34cc02c8dc1
rkdcodus <rcd7275@naver.com>
2024-12-05 13:16:02 +0900

    feat(users.js): 유효성 검사 코드 작성

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const conn = require("../mariadb");
+const { body, param, validationResult } = require("express-validator");
 
 const router = express.Router();
 
@@ -9,68 +10,89 @@ const SQL = {
   delete: "DELETE FROM users WHERE email = ?",
 };
 
+// 미들웨어
+function validator(req, res, next) {
+  const err = validationResult(req);
+
+  if (err.isEmpty()) {
+    return next();
+  }
+  res.status(400).json(err.array());
+}
+
 router.use(express.json());
 
-router.post("/signin", (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/signin",
+  [
+    body("email").notEmpty().isEmail().withMessage("이메일 입력 필요"),
+    body("password").notEmpty().isString().withMessage("비밀번호 입력 필요"),
+    validator,
+  ],
+  (req, res) => {
+    const { email, password } = req.body;
 
-  if (!email && !password) return res.status(400).json({ message: "이메일를 입력해주세요." });
-  if (!password) return res.status(400).json({ message: "비밀번호를 입력해주세요." });
+    conn.query(SQL.select, email, (_, result) => {
+      const user = result[0];
 
-  conn.query(SQL.select, email, (_, result) => {
-    const user = result[0];
-
-    if (user && password === result[0].password) {
-      res.status(200).json({ message: `${result[0].name}님 로그인 되었습니다.` });
-    } else {
+      if (user && password === result[0].password) {
+        return res.status(200).json({ message: `${result[0].name}님 로그인 되었습니다.` });
+      }
       res.status(404).json({ message: "이메일 또는 비밀번호를 잘못 입력했습니다." });
-    }
-  });
-});
+    });
+  }
+);
 
-router.post("/join", (req, res) => {
-  const { email, name, password, connect } = req.body;
+router.post(
+  "/join",
+  [
+    body("email").notEmpty().isEmail().withMessage("이메일 입력 필요"),
+    body("password").notEmpty().isString().withMessage("비밀번호 입력 필요"),
+    body("name").notEmpty().isString().withMessage("이름 입력 필요"),
+    body("connect").isMobilePhone().withMessage("연락처 입력 필요"),
+    validator,
+  ],
+  (req, res) => {
+    const { email, name, password, connect } = req.body;
 
-  if (email && name && password) {
     conn.query(SQL.insert, [email, name, password, connect], (err) => {
       if (err) {
-        res.status(409).json({ message: "이미 사용 중인 아이디입니다." });
-      } else {
-        res.status(201).send({ message: `${name}님 회원가입 되었습니다.` });
+        return res.status(409).json({ message: "이미 사용 중인 아이디입니다." });
       }
+      res.status(201).send({ message: `${name}님 회원가입 되었습니다.` });
     });
-  } else {
-    res.status(400).json({ message: "잘못된 요청입니다." });
   }
-});
+);
 
 router
   .route("/users")
-  .get((req, res) => {
-    const { email } = req.body;
+  .get(
+    [body("email").notEmpty().isEmail().withMessage("이메일 입력 필요"), validator],
+    (req, res) => {
+      const { email } = req.body;
 
-    conn.query(SQL.select, email, (_, result) => {
-      if (result.length) {
-        res.status(200).json(result);
-      } else {
+      conn.query(SQL.select, email, (_, result) => {
+        if (result.length) {
+          return res.status(200).json(result);
+        }
         res.status(404).json({ message: "회원 정보를 찾을 수 없습니다." });
-      }
-    });
-  })
-  .delete((req, res) => {
-    const { email } = req.body;
+      });
+    }
+  )
+  .delete(
+    [body("email").notEmpty().isEmail().withMessage("이메일 입력 필요"), validator],
+    (req, res) => {
+      const { email } = req.body;
 
-    if (!email) return res.status(400).json({ message: "이메일을 입력해주세요" });
-
-    conn.query(SQL.select, email, (_, result) => {
-      if (!result.length) {
-        conn.query(SQL.delete, email, () => {
-          res.status(200).json({ message: `정상적으로 회원 탈퇴되었습니다.` });
-        });
-      } else {
+      conn.query(SQL.select, email, (_, result) => {
+        if (!result.length) {
+          return conn.query(SQL.delete, email, () => {
+            res.status(200).json({ message: `정상적으로 회원 탈퇴되었습니다.` });
+          });
+        }
         res.status(404).json({ message: "회원 정보를 찾을 수 없습니다." });
-      }
-    });
-  });
+      });
+    }
+  );
 
 module.exports = router;
