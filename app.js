2ad23eabc0b0056f567a0fa5fcca37bebfe4df81
rkdcodus <rcd7275@naver.com>
2024-12-05 18:37:12 +0900

    refactor(app.js): 포트번호 env 파일로 이동

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 const app = express();
 
-app.listen(8888);
+dotenv.config();
+app.listen(process.env.PORT);
 
 const userRouter = require("./routes/users");
 const channelRouter = require("./routes/channels");
