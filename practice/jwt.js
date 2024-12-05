5c9d2c985ee6849070fe3285b5927fdb2830ab91
rkdcodus <rcd7275@naver.com>
2024-12-05 18:34:03 +0900

    feat(jwt.js): 암호키 env 파일로 이동

--- practice/jwt.js
+++ practice/jwt.js
@@ -1,8 +1,12 @@
 const jwt = require("jsonwebtoken");
-const token = jwt.sign({ foo: "bar" }, "shhh");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const token = jwt.sign({ foo: "bar" }, process.env.PRIVATE_KEY);
 console.log(token);
 
 // 검증
-const decoded = jwt.verify(token, "shhh");
+const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
 
 console.log(decoded);
